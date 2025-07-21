26cb3742819a5e706ef50dfa8703e05eefdf5060
izaz97778 <izaz97778@gmail.com>
2025-07-21 12:05:45 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,21 +5,19 @@
 from os import environ
 
 class Config:
-    try:
-        API_ID = int(environ["API_ID"])
-        BOT_OWNER = int(environ["BOT_OWNER"])
-    except KeyError as e:
-        raise ValueError(f"Missing required environment variable: {e.args[0]}")
-    except ValueError as e:
-        raise ValueError("API_ID and BOT_OWNER must be integers.")
-
+    API_ID = int(environ.get("API_ID", ""))
     API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "")
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
+    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
 
-class temp(object): 
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+class temp(object):
     lock = {}
     CANCEL = {}
     forwardings = 0
